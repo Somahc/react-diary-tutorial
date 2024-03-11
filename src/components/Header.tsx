3be5c39cd1f407deefcec89f7ae9294312dd8f8c
Soma Sekine <a8121151@aoyama.jp>
2024-03-11 18:16:32 -0400

    ヘッダーのログインボタン

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom"; 
 
 const Header = () => {
-  return (
+
+    const user: boolean = false;
+
+    return (
     <header className="bg-white">
         <div className="container flex justify-between items-center mx-auto px-2 h-20">
             <h1><Link to="/" className="md:text-4xl text-3xl">Short Diary</Link></h1>
@@ -9,13 +12,19 @@ const Header = () => {
                 <ul className="flex gap-2 items-center">
                     <li className="hidden lg:block"><Link to="/create">日記を書く</Link></li>
                 </ul>
-                <div>
-                    <img src="/user-icon.png" alt="" className="w-10 h-10 rounded-full" />
-                </div>
+                {user ?
+                    (<div className="flex gap-3 items-center">
+                        <div>
+                            <img src={user.photoURL as string} alt="" className="w-10 h-10 rounded-full" />
+                        </div>
+                        <button className="bg-black text-white py-[7px] px-4 rounded-md">Logout</button>
+                    </div>) :
+                    (<button className="bg-black text-white py-[7px] px-4 rounded-md"><Link to="login">Login</Link></button>)
+                }
             </nav>
         </div>
     </header>
-  );
+    );
 };
 
 export default Header;
